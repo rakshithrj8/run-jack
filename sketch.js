a2354fae03f0bc82128107f8104f2e377920cf6d
rakshithrj8 <85885550+rakshithrj8@users.noreply.github.com>
2021-06-27 11:05:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,39 @@
-//<<<<<<<<<<<creating variables......by rj rakshith...............>>>>>>>>
-var  ship,ship_running;
-var sea,seaImage;
-//arraying the animations......
-function preload(){
-  ship_running = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
-  groundImage = loadImage("sea.png");
+
+ var rj,rj_running;
+ var road,roadImage;
+
+
+ function preload(){
+  //pre-load images
+  rj_running = loadAnimation("Runner-1.png","Runner-2.png");
+  roadImage = loadImage("path.png")
 }
 
 function setup(){
-  createCanvas(2000,400);
-  //creating ship and adding animation.........
- ship = createSprite(900,200,20,20);
- ship.addAnimation("running",ship_running);
-//craeting sea and animation....... 
-sea = createSprite(690,200,500,400);
-sea.addImage(groundImage);
-ship.scale=0.5;
-sea.scale=0.3;
-sea.depth = ship.depth
-ship.depth = ship.depth + 1;
+  createCanvas(400,400);
+  //create sprites here
+rj = createSprite(200,300,20,20);
+road = createSprite(200,50,200,700);
+rj.addAnimation("running",rj_running);
+road.addImage(roadImage);
+road.scale=1.2;
+rj.scale=0.1;
+road.depth = rj.depth
+rj.depth = rj.depth + 1;
 }
 
-function draw() {
-  background("blue");
-
-drawSprites();      }
+  function draw() {
+    background(0);
+   background("white");
+   if(keyDown("right")){
+    rj.velocityX=4;
+  }
+  if(keyDown("left")){
+    rj.velocityX=-4;
+  }
+  
+ 
+  road.velocityY=9;
+  console.log(road.y);
+ 
+drawSprites();}   
